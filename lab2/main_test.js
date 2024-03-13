a4a24bd93a02cc4343b1ca25bd9d0fab7a44b105
Joshua <joshua87327@gmail.com>
2024-03-14 01:18:51 +0800

    Eigth Attempt on LAB2

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -22,10 +22,10 @@ test("MailSystem send()", (t) => {
     assert.strictEqual(sendStatus, false);
 });
 
-const nameList = ['Joshua','Ruby'];
+const nameList = ['Joshua','Ruby','Heidi'];
 
 test("Application getNames()", async (t) => {
-    fs.writeFileSync('name_list.txt', 'Joshua\nRuby', 'utf8');
+    fs.writeFileSync('name_list.txt', 'Joshua\nRuby\nHeidi', 'utf8');
     const app = new Application();
     getNameList = await app.getNames();
     assert.deepStrictEqual(getNameList, [nameList, []]);
@@ -37,9 +37,12 @@ test("Application getRandomPerson()",async (t) => {
     Math.random = () => 0;
     let rndPerson = await app.getRandomPerson();
     assert.strictEqual(rndPerson, 'Joshua');
-    Math.random = () => 0.5;
+    Math.random = () => 0.6;
     rndPerson = await app.getRandomPerson();
     assert.strictEqual(rndPerson, 'Ruby');
+    Math.random = () => 0.9;
+    rndPerson = await app.getRandomPerson();
+    assert.strictEqual(rndPerson, 'Heidi');
 });
 
 test("Application selectNextPerson()", async (t) => {
@@ -51,10 +54,15 @@ test("Application selectNextPerson()", async (t) => {
     assert.strictEqual(next, 'Joshua');
     assert.deepStrictEqual(app.selected, ['Joshua']);
 
-    Math.random = () => 0.5;
+    Math.random = () => 0.6;
     next = app.selectNextPerson();
     assert.strictEqual(next, 'Ruby');
-    assert.deepStrictEqual(app.selected, ['Joshua', 'Ruby']);
+    assert.deepStrictEqual(app.selected, ['Joshua','Ruby']);
+
+    Math.random = () => 0.9;
+    next = app.selectNextPerson();
+    assert.strictEqual(next, 'Heidi');
+    assert.deepStrictEqual(app.selected, ['Joshua','Ruby','Heidi']);
 
     assert.strictEqual(app.selectNextPerson(), null);
     
@@ -62,24 +70,18 @@ test("Application selectNextPerson()", async (t) => {
 
 test("Application notifySelected()", async (t) => {
 
-    const writeSpy = t.mock.fn(() => {true});
-    t.mock.method(MailSystem.prototype, 'write').mock.mockImplementation(writeSpy);
-
-    const sendSpy = t.mock.fn(() => {true});
-    t.mock.method(MailSystem.prototype, 'send').mock.mockImplementation(sendSpy);
-    
     const app = new Application();
     await app.getNames();
-    Math.random = () => 0;
-    app.selectNextPerson();
-    Math.random = () => 0.5;
-    app.selectNextPerson();
+    app.people = ['Joshua','Ruby','Heidi'];
+    app.selected = ['Joshua','Ruby','Heidi'];
+    app.mailSystem.send = test.mock.fn(app.mailSystem.send);
+    app.mailSystem.write = test.mock.fn(app.mailSystem.write);
     
 
     app.notifySelected();
     
-    assert.strictEqual(writeSpy.mock.calls.length, 2);
-    assert.strictEqual(sendSpy.mock.calls.length, 2);
+    assert.strictEqual(app.mailSystem.send.mock.calls.length, 3);
+    assert.strictEqual(app.mailSystem.write.mock.calls.length, 3);
 
     fs.unlinkSync('name_list.txt');
 });
