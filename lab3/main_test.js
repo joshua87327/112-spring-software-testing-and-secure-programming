7850da417cd0c48a118014b73be1d2d7a0093d15
Joshua <joshua87327@gmail.com>
2024-03-27 19:56:30 +0800

    Reattemps

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -9,12 +9,15 @@ test('Test Calculator', () => {
     // print(Math.exp(1.8E+308));
     const testcases_exp = [
         { param: 1/0, expected: { name: 'Error', message: 'unsupported operand type'} },
+        { param: 'a', expected: { name: 'Error', message: 'unsupported operand type'} },
         { param: 1.8E+307, expected: { name: 'Error', message: 'overflow'} },
     ];
     for(const tc of testcases_exp){
         assert.throws(()=> { calc.exp(tc.param) }, tc.expected);
     }
+    assert.strictEqual(calc.exp(0), Math.exp(0));
     assert.strictEqual(calc.exp(1), Math.exp(1));
+    assert.doesNotThrow(() => { calc.exp(700) });
 
     const testcases_log = [
         { param: 1/0, expected: { name: 'Error', message: 'unsupported operand type'} },
@@ -25,5 +28,6 @@ test('Test Calculator', () => {
         assert.throws(()=> { calc.log(tc.param) }, tc.expected);
     }
     assert.strictEqual(calc.log(1), Math.log(1));
+    assert.strictEqual(calc.log(Math.E), Math.log(Math.E));
 
 });
