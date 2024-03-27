8359366cd7f1f3e04c289c86076cfca59277f910
Joshua <joshua87327@gmail.com>
2024-03-27 19:18:11 +0800

    Attempt For LAB 3

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,5 +1,29 @@
+const test = require('node:test');
 const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
 // TODO: write your tests here
+test('Test Calculator', () => {
+    const calc = new Calculator();
+    // print(Math.exp(1.8E+308));
+    const testcases_exp = [
+        { param: 1/0, expected: { name: 'Error', message: 'unsupported operand type'} },
+        { param: 1.8E+307, expected: { name: 'Error', message: 'overflow'} },
+    ];
+    for(const tc of testcases_exp){
+        assert.throws(()=> { calc.exp(tc.param) }, tc.expected);
+    }
+    assert.strictEqual(calc.exp(1), Math.exp(1));
+
+    const testcases_log = [
+        { param: 1/0, expected: { name: 'Error', message: 'unsupported operand type'} },
+        { param: -0, expected: { name: 'Error', message: 'math domain error (1)'} },
+        { param: -1, expected: { name: 'Error', message: 'math domain error (2)'} },
+    ];
+    for(const tc of testcases_log){
+        assert.throws(()=> { calc.log(tc.param) }, tc.expected);
+    }
+    assert.strictEqual(calc.log(1), Math.log(1));
+
+});
